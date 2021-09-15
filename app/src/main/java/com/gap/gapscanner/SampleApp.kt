e14da5dd2365c6f52a9e5727aad318a6eada0b1c
Eric Choy <Eric_Pan_Choy@gap.com>
2021-09-14 17:13:34 -0700

    CPODS-1304 [EChoy] Sample App and Sample Library

---
+++ app/src/main/java/com/gap/gapscanner/SampleApp.kt
@@ -0,0 +1,25 @@
+package com.gap.gapscanner
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.gap.gapscannerlibrary.TestItem
+
+class SampleApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sample_app)
+
+        val push = findViewById<Button>(R.id.pushBtn)
+        val text = findViewById<TextView>(R.id.displayTxt)
+
+        var name = TestItem("George")
+
+        push.setOnClickListener() {
+            text.setText("Hello " + name.getName())
+            Log.d("---------Name---------", name.getName())
+        }
+    }
+}
